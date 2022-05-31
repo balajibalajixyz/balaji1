7fa39bb2b55786df9bdb0189671668ab24bfa673
balajibalajixyz <106381071+balajibalajixyz@users.noreply.github.com>
2022-05-31 09:13:06 +0530

    Update javaprogram.java

--- javaprogram.java
+++ javaprogram.java
@@ -1,11 +1,15 @@
 package com.hellowworld;
 
 public class javaprogram {
-
-
+static String="java";
+public static  void selenium(){
+	System.out.println("selenium")
+}
 	public static void main(String[] args) {
 		int balaji =10
 	System.out.println("balaji");
+	selenium();
+	System.out.println(javaprogram.string)
 	}
 
 }
